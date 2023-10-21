ca8dea02e06b2c0240047649e393209de7bdd470
suyash002 <getontosuyash@gmail.com>
2023-10-21 17:34:43 +0530

    regex placeapi google

--- src/utils.js
+++ src/utils.js
@@ -46,6 +46,67 @@ export const stringes = {
   invalidMonth: "Invalid Month",
   invalidcity: "Invalid City",
 };
+// *******************************************************
+// Mobile Number validation
+export function mobileNumberValid(MobileNumber) {
+  var mob = /^[6-9]{1}[0-9]{9}$/;
+  if (mob.test(MobileNumber) === false) {
+    return false;
+  }
+  return true;
+}
+
+// Hide Mobile Number
+export function hideMobileNumber(MobileNumber) {
+  if (MobileNumber.length === 10) {
+    return (
+      MobileNumber.slice(0, 2) + MobileNumber.slice(2).replace(/.(?=...)/g, "*")
+    );
+  }
+  return null;
+}
+
+// email address vaildation
+export function emailAddressValid(emailAddress) {
+  return emailAddress.match(
+    /^([A-Z?a-z0-9_\-\.])+\@([A-Za-z_\-\.])+\.([A-Za-z]{2,4})$/
+  );
+}
+
+// Aadhar validation
+// export function validateAadhaar(ano) {
+//   var regexp = /^[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}$/;
+//   if (regexp.test(ano)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// Pancard validation
+// export const pancardValidation = (text) => {
+//   let regex = /^([A-Z]){5}([0-9]){4}([A-Z]){1}?$/;
+//   if (regex.test(text)) {
+//     return true;
+//   }
+
+//   return false;
+// };
+
+// name validation
+export function nameValid(name) {
+  // return name.match(/^[a-zA-Z ]{2,30}$/);
+  return name.match(/^[A-Za-z0-9\s]{1,30}$/);
+}
+
+
+// GST_number_validation
+export function gstNumberValid(gstNumber) {
+  return gstNumber.match(
+    /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/
+  );
+}
+// *******************************************************
 
 
 const loadRazorpay = async () => {
